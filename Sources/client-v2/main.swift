ee5451d7d3595591bba67864e3d126f13c30ca8a
ja.toctaguano <ja.toctaguano@alumnos.urjc.es>
2020-11-15 21:40:53 +0100

    mods

--- Sources/client-v2/main.swift
+++ Sources/client-v2/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+
+import Socket
+import Foundation
+import Glibc
+
+let server = "localhost"
+let port: Int32 = 7667
+
+struct Point {
+    var x: Double
+    var y: Double
+}
+
+do {
+    guard let serverAddress = Socket.createAddress(for: server, on: port) else {
+    print("Error creating Address")
+    exit(1)
+}
+
+    let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+    var buffer = Data(capacity: 1000)
+    "hello".utf8CString.withUnsafeBytes { buffer.append(contentsOf: $0) }
+    withUnsafeBytes(of: 42) { buffer.append(contentsOf: $0) }
+    withUnsafeBytes(of: Point(x: 0.7, y: -1.25)) { buffer.append(contentsOf: $0) }
+    try clientSocket.write(from: buffer, to: serverAddress)
+} catch let error {
+    print("Connection error: \(error)")
+}
